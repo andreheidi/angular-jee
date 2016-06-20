d89f45674cb752f850ee4a6bbf05788b6bdd4e01
Andre Moriya <andremoriya@gmail.com>
2016-06-20 19:02:18 -0300

    Add generic parameter

--- src/main/java/me/moriya/repository/producer/RepositoryFactory.java
+++ src/main/java/me/moriya/repository/producer/RepositoryFactory.java
@@ -1,21 +1,25 @@
 package me.moriya.repository.producer;
 
+import me.moriya.entity.Model;
 import me.moriya.repository.GenericRepository;
 import me.moriya.repository.Repository;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Named;
+import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 
 /**
  * Created by andre on 6/7/16.
  */
+@Named
 public final class RepositoryFactory {
 
     @Produces
-    public GenericRepository createRepository(InjectionPoint injectionPoint) {
+    public <T extends Model<ID>, ID extends Serializable> GenericRepository<T, ID> createRepository(InjectionPoint injectionPoint) {
         ParameterizedType type = (ParameterizedType) injectionPoint.getType();
         Class classe = (Class) type.getActualTypeArguments()[0];
-        return new Repository(classe);
+        return new Repository<T, ID>(classe);
     }
 }
